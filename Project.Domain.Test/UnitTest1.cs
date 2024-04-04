902284d381ed5934d405424f7f2d4dd2f0cde28b
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-04-05 00:01:43 +0600

    Add project files.

---
+++ Project.Domain.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Project.Domain.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Project.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Project.Infrastructure.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
