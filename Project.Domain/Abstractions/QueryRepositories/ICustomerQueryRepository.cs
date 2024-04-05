c2a5e22cd9ce8a1fe068639ab904469b8f6cc319
Mamunur Rushid <mamunurrushid60@gmail.com>
2024-04-05 13:07:58 +0600

    initial project structure completed (5 April 2024)

---
+++ Project.Domain/Abstractions/QueryRepositories/ICustomerQueryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Project.Domain.Abstractions.QueryRepositories.Base;
+using Project.Domain.Entities;
+
+namespace Project.Domain.Abstractions.QueryRepositories
+{
+    public interface ICustomerQueryRepository : IQueryRepository<Customer>
+    {
+        Task<Customer> GetCustomerByEmail(string email);
+    }
+}
